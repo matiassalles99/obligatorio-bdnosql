718a805362dede200dae77c85ada21bf39d9aefd
matiassalles99 <matiassalles99@gmail.com>
2022-07-02 17:42:52 -0300

    change guid into city

--- obligatorio/app/models/vehicle_telemetry.rb
+++ obligatorio/app/models/vehicle_telemetry.rb
@@ -2,19 +2,18 @@ class VehicleTelemetry
   include ActiveModel::Model
   include Cassandra::Model
 
-  attributes :guid, :pressure, :velocity, :wind_velocity, :captured_at
+  attributes :city, :pressure, :velocity, :wind_velocity, :captured_at
   table_name 'vehicleTelemetry'
 
-  validates :pressure, :velocity, :wind_velocity, presence: true
+  validates :city, :pressure, :velocity, :wind_velocity, presence: true
 
   class << self
     def fillable
-      %i[pressure velocity wind_velocity]
+      %i[city pressure velocity wind_velocity]
     end
   end
 
   def prepare_for_cassandra_insert
-    self.guid = SecureRandom.uuid[0,5]
     self.captured_at = Time.zone.now
     self.pressure = pressure.to_f
     self.velocity = velocity.to_f
